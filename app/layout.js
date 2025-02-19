9e9e337ff1096c98c18c18d30978d940a415af00
Rial <rialparmar007@gmail.com>
2025-02-19 14:18:06 -0800

    creating frontend food-ordering app from scratch

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Footer />
       </body>
     </html>
   );
